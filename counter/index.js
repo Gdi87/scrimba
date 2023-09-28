a7691cf0e00b8dc8ad550ef47286992624d17d73
gdi87 <alfredo.gdbl@gmail.com>
2023-09-28 13:48:47 +0200

    sec

--- counter/index.js
+++ counter/index.js
@@ -1,11 +1,14 @@
+let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
 
 function increment() {
     count += 1
-    countEl.innerText = count
+    countEl.textContent = count
 }
 
 function save() {
-    console.log(count)
+    saveEl.textContent += count + " - "
+    countEl.textContent = 0
+    count = 0
 }
