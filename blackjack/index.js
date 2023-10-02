9cf5a6dc1b0fedfa89b57662428354d56346939f
gdi87 <alfredo.gdbl@gmail.com>
2023-10-02 19:09:03 +0200

    .
    .

--- blackjack/index.js
+++ blackjack/index.js
@@ -1,31 +1,25 @@
-let firstCard = 10;
-let secondCard = 11;
-let sum = firstCard + secondCard;
-let hasBlackJack = false;
-let isAlive = true;
-let message = "";
+let firstCard = 10
+let secondCard = 11
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
 
-let messageEl = document.getElementById("message-el").textContent;
-console.log(messageEl)
+let messageEl = document.getElementById("message-el")
 
-let checkGameStatus = () => {
-    if (sum <= 20) {
-        message = "Do you want to draw a new card? 🙂";
-    } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! 🥳";
-        hasBlackJack = true;
-    } else {
-        message = "You're out of the game! 😭";
-        isAlive = false;
-    }
-}
 
 function startGame() {
-    checkGameStatus();
-    messageEl.textContent = message;
-
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
     console.log(message);
     console.log(hasBlackJack);
     console.log(isAlive);
 }
-startGame();
--- blackjack/textt.js
+++ blackjack/textt.js
@@ -49,3 +49,35 @@ console.log(3 < 0)    //f
 console.log(3 >= 3)   // t
 console.log(11 <= 11) //t
 console.log(3 <= 2)   //f
+
+/*let firstCard = 10;
+let secondCard = 11;
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+
+let messageEl = document.getElementById("message-el");
+
+let checkGameStatus = () => {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂";
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack! 🥳";
+        hasBlackJack = true;
+    } else {
+        message = "You're out of the game! 😭";
+        isAlive = false;
+    }
+}
+
+
+function startGame() {
+    checkGameStatus();
+    messageEl.textContent = message;
+
+    console.log(message);
+    console.log(hasBlackJack);
+    console.log(isAlive);
+}
+startGame(); */
