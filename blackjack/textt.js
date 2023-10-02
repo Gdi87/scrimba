f8d2206b3cb6145e6967abd1095de8ee738a17ec
gdi87 <alfredo.gdbl@gmail.com>
2023-10-02 11:38:32 +0200

    .
    .

---
+++ blackjack/textt.js
@@ -0,0 +1,43 @@
+let firstCard = Math.random(2 - 11)
+let secondCard = 2
+
+let sum = firstCard + secondCard
+console.log(sum)
+
+if (sum < 21) {
+    console.log("Do you want to draw a new card?")
+} else if (sum === 21) {
+    console.log("u W🙂N")
+} else if (sum > 21) {
+    console.log("losah😭")
+} else {
+    console.log("u r braking the phisics and the space and the infinite!!!")
+};
+
+let age = 22
+if (age < 21) {
+    console.log("You can not enter here!")
+} else {
+    console.log("Welcomen!")
+}
+if (age < 100) {
+    console.log("Not elegible")
+} else if (age === 100) {
+    console.log("Here is your birthday card from the King!")
+} else {
+    console.log("Not elegible, you have already gotten one")
+}
+
+let firstCaard = 10
+let secondCaard = 7
+let suum = firstCaard + secondCaard
+
+if (suum <= 20) {
+    console.log("Do you want to draw a new card? 🙂")
+}
+else if (suum === 21) {
+    console.log("Wohoo! You've got Blackjack! 🥳")
+}
+else {
+    console.log("You're out of the game! 😭")
+};
