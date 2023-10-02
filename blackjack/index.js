8a0aa2c343f9237ab788840413a33df6291d4251
gdi87 <alfredo.gdbl@gmail.com>
2023-10-02 17:29:52 +0200

    .
    .

--- blackjack/index.js
+++ blackjack/index.js
@@ -1,22 +1,31 @@
-let firstCard = 10
-let secondCard = 11
-let sum = firstCard + secondCard
-let hasBlackJack = false
-console.log(sum)
-let isAlive = true
-let message = ""
+let firstCard = 10;
+let secondCard = 11;
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂"
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game! 😭"
-    isAlive = false
+let messageEl = document.getElementById("messageel")
+
+let checkGameStatus = () => {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂";
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack! 🥳";
+        hasBlackJack = true;
+    } else {
+        message = "You're out of the game! 😭";
+        isAlive = false;
+    }
 }
 
-console.log(message)
-console.log(hasBlackJack)
-console.log(isAlive)
-    ;
+function startGame() {
+    checkGameStatus();
+    messageEl.textContent = message;
+
+    console.log(message);
+    console.log(hasBlackJack);
+    console.log(isAlive);
+}
+
+startGame();
